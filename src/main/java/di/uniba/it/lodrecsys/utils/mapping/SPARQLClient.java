720338e4964a43fd30945b4676560ac36fb855da
asuglia <alessandro.suglia@gmail.com>
2014-06-16 23:08:09 +0200

    Resources' properties retriever

--- src/main/java/di/uniba/it/lodrecsys/utils/mapping/SPARQLClient.java
+++ src/main/java/di/uniba/it/lodrecsys/utils/mapping/SPARQLClient.java
@@ -8,8 +8,7 @@ import di.uniba.it.lodrecsys.utils.Utils;
 import org.apache.jena.atlas.web.HttpException;
 
 import java.io.IOException;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -23,52 +22,81 @@ public class SPARQLClient {
     private String graphURI = "http://dbpedia.org";
     private static Logger currLogger = Logger.getLogger(SPARQLClient.class.getName());
 
-    public void exec(String resource, String prop) {
-        this.resource = resource;
-        this.property = prop;
-        Query query;
-        String q;
+    private String formatPropertiesList(Collection<String> specificProp) {
+        String formattedProperties = "";
 
-        String resourceQuery = "<" + resource + ">";
-        String propQuery = "<" + prop + ">";
-        // creation of a mapping query for getting all the resources connected to resource
-        //the FILTER isIRI is used to get only resources, so this query descards any literal or data-type
+        for (String prop : specificProp)
+            formattedProperties += prop + " ";
 
-        q = " SELECT * WHERE {{" + " ?s " + propQuery + " " + resourceQuery
-                + ". " + "FILTER isIRI(?s). " + " } UNION {" + resourceQuery + " "
-                + propQuery + " ?o " + "FILTER isIRI(?o). " + "}}";
-        try {
-            query = QueryFactory.create(q);
-
-            execQuery(query);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        return formattedProperties;
 
     }
 
-    public void exec(String resource) {
-        this.resource = resource;
-        Query query;
-        String q;
 
-        String resourceQuery = "<" + resource + ">";
-        // creation of a mapping query for getting all the resources connected to resource
-        //the FILTER isIRI is used to get only resources, so this query descards any literal or data-type
+    public Map<String, String> getURIProperties(String uri, Collection<String> specificProp) {
+        Map<String, String> propertiesURI = new HashMap<>();
+        String proprVariable = "?prop",
+                valueVariable = "?value",
+                fixedURI = "<" + uri + ">",
+                queryProp = "select " + proprVariable + " where {\n" +
+                        fixedURI + " " + proprVariable + " " + valueVariable +
+                        "values " + proprVariable + "{" +
+                        formatPropertiesList(specificProp) + "} \n" +
+                        "}";
 
-        q = " SELECT * WHERE {{" + " ?s ?p " + resourceQuery
-                + ". " + "FILTER isIRI(?s). " + " } UNION {" + resourceQuery +
-                " ?p ?o " + "FILTER isIRI(?o). " + "}}";
+        currLogger.info(queryProp);
+        Query query = QueryFactory.create(queryProp);
+
+        QueryExecution qexec = null;
         try {
-            query = QueryFactory.create(q);
 
-            execQuery(query);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+
+            boolean doneIt = false;
+
+            while (!doneIt) {
+
+                try {
+                    if (graphURI == null)
+                        qexec = QueryExecutionFactory.sparqlService(endpoint, query);
+                        //qexec = QueryExecutionFactory.sparqlService(newEndpoint, query);
+                    else
+                        qexec = QueryExecutionFactory.sparqlService(endpoint, query,
+                                graphURI);
+
+                    ResultSet resultSet = qexec.execSelect();
+
+                    currLogger.info("Executed query!");
+
+                    QuerySolution currSolution;
+
+                    while (resultSet.hasNext()) {
+                        currSolution = resultSet.nextSolution();
+
+                        propertiesURI.put(currSolution.getResource(proprVariable).toString(), currSolution.getLiteral(valueVariable).toString());
+
+                    }
+
+                    myWait(30);
+                    doneIt = true;
+                } catch (Exception ex) {
+
+                    doneIt = false;
+                    currLogger.fine("Try again...");
+                    propertiesURI.clear();
+                    myWait(30);
+
+
+                }
+            }
+            return propertiesURI;
+
+        } finally {
+            if (qexec != null)
+                qexec.close();
         }
 
-    }
 
+    }
 
     public Set<String> getURIProperties(String uri) {
         Set<String> propertiesURI = new TreeSet<>();
@@ -249,75 +277,5 @@ public class SPARQLClient {
 
     }
 
-    private void execQuery(Query query) {
 
-        System.out.println("executing query : " + query.toString());
-
-        QueryExecution qexec = null;
-        try {
-            if (graphURI == null)
-                qexec = QueryExecutionFactory.sparqlService(endpoint, query);
-            else
-                qexec = QueryExecutionFactory.sparqlService(endpoint, query,
-                        graphURI);
-
-            ResultSet results = qexec.execSelect();
-
-            QuerySolution qs;
-            RDFNode node, prop;
-
-            String n = "", p = this.property;
-
-            System.out.println("Results:");
-            //iteration over the resultset
-            while (results.hasNext()) {
-
-                qs = results.next();
-
-                if (qs.contains("p")) {
-                    prop = qs.get("p"); //get the predicate of the triple
-                    p = prop.toString();
-                    p = p.replace("<", "");
-                    p = p.replace(">", "");
-
-                }
-                if (qs.get("o") == null) {
-                    node = qs.get("s"); //get the subject of the triple
-                    n = node.toString();
-                    n = n.replace("<", "");
-                    n = n.replace(">", "");
-
-                    System.out.println(n + '\t' + p + '\t' + resource);
-                } else {
-
-                    node = qs.get("o"); //get the object of the triple
-                    n = node.toString();
-                    n = n.replace("<", "");
-                    n = n.replace(">", "");
-
-                    System.out.println(resource + '\t' + p + '\t' + n);
-
-                }
-
-            }
-
-        } finally {
-            if (qexec != null)
-                qexec.close();
-        }
-
-    }
-
-    public static void main(String[] args) {
-
-        SPARQLClient exec = new SPARQLClient();
-        //get all the triples related to the predicate http://dbpedia.org/ontology/starring
-        //wherein the Godfather appears as subject or object
-        exec.exec("http://dbpedia.org/resource/The_Godfather", "http://dbpedia.org/ontology/starring");
-
-        //get all the triples that involve the Godfather
-        //exec.exec("http://dbpedia.org/resource/The_Godfather");
-
-
-    }
 }
