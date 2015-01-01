e5e49c928909e6709326f5a01098103e6c4a04d9
simoruty <simonerutigliano88@gmail.com>
2015-01-01 14:20:06 +0100

    multithreading fix

--- src/main/java/di/uniba/it/lodrecsys/graph/GraphRunner.java
+++ src/main/java/di/uniba/it/lodrecsys/graph/GraphRunner.java
@@ -1,13 +1,12 @@
 package di.uniba.it.lodrecsys.graph;
 
 import di.uniba.it.lodrecsys.eval.SparsityLevel;
+import di.uniba.it.lodrecsys.utils.LoadProperties;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Date;
 
-import static di.uniba.it.lodrecsys.graph.RecommenderSys.loadValue;
+import static di.uniba.it.lodrecsys.graph.RecommenderSys.*;
 
 /**
  * Starts all the graph-based experiments and evaluate them
@@ -19,9 +18,50 @@ public class GraphRunner {
 
         loadValue();
 
-        for (SparsityLevel level : SparsityLevel.values())
-            new RecommenderSys(level.toString()).start();
+        for (SparsityLevel level : SparsityLevel.values()) {
+//            new RecommenderSys(level.toString()).start();
+            for (int numSplit = 1; numSplit <= LoadProperties.NUMSPLIT; numSplit++) {
 
+//            SplitThread e = new SplitThread(level, numSplit);
+//            e.start();
+//            while (e.isAlive()){
+//            }
+
+                String trainFile = LoadProperties.TRAINPATH + File.separator +
+                        level + File.separator +
+                        "u" + numSplit + ".base";
+
+                String testFile = LoadProperties.TESTPATH + File.separator +
+                        "u" + numSplit + ".test";
+
+                try {
+                    featureSelection(trainFile, testFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                savefileLog("***************************************************");
+                savefileLog("***    Recommender with pagerank algorithm      ***");
+                savefileLog("***************************************************");
+                savefileLog("");
+                savefileLog(new Date() + " [INFO] Inizialized computing recommendations for split #" + numSplit + " level: " + level + " ...");
+
+                try {
+                    recommendations(trainFile, testFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //        LOGGERGRAPHRUNNER.info("Computed recommendations for split #" + numSplit + " level: " + level);
+                savefileLog(new Date() + " [INFO] Computed recommendations for split #" + numSplit + " level: " + level);
+                savefileLog("-----------------------------------------------------");
+            }
+
+            try {
+                evaluator(level.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void savefileLog(String s) {
