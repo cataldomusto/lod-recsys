84273650ae6c3edf6076617910d6239145c702a5
simoruty <simonerutigliano88@gmail.com>
2015-01-04 22:47:55 +0100

    Result mrmr and Pagerank

--- src/main/java/di/uniba/it/lodrecsys/graph/GraphRunner.java
+++ src/main/java/di/uniba/it/lodrecsys/graph/GraphRunner.java
@@ -20,47 +20,47 @@ public class GraphRunner {
 
         for (SparsityLevel level : SparsityLevel.values()) {
 //            new RecommenderSys(level.toString()).start();
-            for (int numSplit = 1; numSplit <= LoadProperties.NUMSPLIT; numSplit++) {
-
-//            SplitThread e = new SplitThread(level, numSplit);
-//            e.start();
-//            while (e.isAlive()){
+//            for (int numSplit = 1; numSplit <= LoadProperties.NUMSPLIT; numSplit++) {
+//
+////            SplitThread e = new SplitThread(level, numSplit);
+////            e.start();
+////            while (e.isAlive()){
+////            }
+//
+//                String trainFile = LoadProperties.TRAINPATH + File.separator +
+//                        level + File.separator +
+//                        "u" + numSplit + ".base";
+//
+//                String testFile = LoadProperties.TESTPATH + File.separator +
+//                        "u" + numSplit + ".test";
+//
+//                try {
+//                    featureSelection(trainFile, testFile);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                savefileLog("***************************************************");
+//                savefileLog("***    Recommender with pagerank algorithm      ***");
+//                savefileLog("***************************************************");
+//                savefileLog("");
+//                savefileLog(new Date() + " [INFO] Inizialized computing recommendations for split #" + numSplit + " level: " + level + " ...");
+//
+//                try {
+//                    recommendations(trainFile, testFile);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                //        LOGGERGRAPHRUNNER.info("Computed recommendations for split #" + numSplit + " level: " + level);
+//                savefileLog(new Date() + " [INFO] Computed recommendations for split #" + numSplit + " level: " + level);
+//                savefileLog("-----------------------------------------------------");
 //            }
-
-                String trainFile = LoadProperties.TRAINPATH + File.separator +
-                        level + File.separator +
-                        "u" + numSplit + ".base";
-
-                String testFile = LoadProperties.TESTPATH + File.separator +
-                        "u" + numSplit + ".test";
-
-                try {
-                    featureSelection(trainFile, testFile);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                savefileLog("***************************************************");
-                savefileLog("***    Recommender with pagerank algorithm      ***");
-                savefileLog("***************************************************");
-                savefileLog("");
-                savefileLog(new Date() + " [INFO] Inizialized computing recommendations for split #" + numSplit + " level: " + level + " ...");
-
-                try {
-                    recommendations(trainFile, testFile);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                //        LOGGERGRAPHRUNNER.info("Computed recommendations for split #" + numSplit + " level: " + level);
-                savefileLog(new Date() + " [INFO] Computed recommendations for split #" + numSplit + " level: " + level);
-                savefileLog("-----------------------------------------------------");
-            }
-
-            try {
-                saveRec(level.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+//
+//            try {
+//                saveRec(level.toString());
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
             evaluator(level.toString());
         }
     }
