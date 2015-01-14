eda4037f2d78ae7fc127c75c69fdcc7dd524bde4
simoruty <simonerutigliano88@gmail.com>
2015-01-14 17:16:29 +0100

    fix file choosen prop

--- src/main/java/di/uniba/it/lodrecsys/graph/GraphEvalRun.java
+++ src/main/java/di/uniba/it/lodrecsys/graph/GraphEvalRun.java
@@ -2,6 +2,7 @@ package di.uniba.it.lodrecsys.graph;
 
 import di.uniba.it.lodrecsys.utils.LoadProperties;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
@@ -18,17 +19,26 @@ public class GraphEvalRun {
     public static void main(String[] args) throws IOException {
 
         String level = args[0];
-        LoadProperties.FILTERTYPE = args[1];
-        if (!LoadProperties.FILTERTYPE.equals("CFSubsetEval"))
+        String fileProp = LoadProperties.FILTERTYPE = args[1];
+        if (!LoadProperties.FILTERTYPE.equals("CFSubsetEval")) {
             LoadProperties.NUMFILTER = args[2];
-        if (LoadProperties.FILTERTYPE.equals("RankerWeka"))
+            fileProp += args[2];
+        }
+        if (LoadProperties.FILTERTYPE.equals("RankerWeka")) {
             LoadProperties.EVALWEKA = args[3];
+            fileProp += args[3];
+        }
+
+        LoadProperties.CHOOSENPROP = "./mapping/choosen_prop" + fileProp;
 
         System.out.println(new Date() + "[INFO] Evaluation " + level + " Started.");
 
         evaluator(level);
 
         System.out.println(new Date() + "[INFO] Evaluation " + level + " Completed.");
+
+        if (new File(LoadProperties.CHOOSENPROP).exists())
+            new File(LoadProperties.CHOOSENPROP).delete();
     }
 }
 
