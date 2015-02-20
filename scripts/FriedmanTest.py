b11e8b6585e6b1a2f5284c3334a04385c6ef41cb
simoruty <simonerutigliano88@gmail.com>
2015-02-20 22:34:20 +0100

    Friedman with post-hoc

--- scripts/FriedmanTest.py
+++ scripts/FriedmanTest.py
@@ -14,6 +14,8 @@ def FriedmanTestcomparisonFeatures(topN, metrics):
     cmd = "java -cp lodrecsys.jar di.uniba.it.lodrecsys.utils.MakerFriedmanTest comparisonFeatures "+tops
     subprocess.call(cmd, shell=True)    
     dire="./datasets/ml-100k/results/UserItemExpDBPedia/CSV/comparisonFeatures/"
+    if not(os.path.exists("./datasets/ml-100k/results/UserItemExpDBPedia/results")):
+        os.makedirs("./datasets/ml-100k/results/UserItemExpDBPedia/results")
     cmd = "Rscript ./scripts/RcomparisonFeaturesFriedman "+dire+" ./datasets/ml-100k/results/UserItemExpDBPedia/results/ComparisonFeaturesFriedman"
     subprocess.call(cmd, shell=True)
     print time.strftime("%Y-%m-%d %H:%M") + " Comparison features finished."
@@ -27,7 +29,8 @@ def FriedmanTestcomparisonAlg(allalg, allalgWEKA, metrics):
     
     cmd = "java -cp lodrecsys.jar di.uniba.it.lodrecsys.utils.MakerFriedmanTest comparisonAlg "+allAlg
     subprocess.call(cmd, shell=True)    
-    
+    if not(os.path.exists("./datasets/ml-100k/results/UserItemExpDBPedia/results")):
+        os.makedirs("./datasets/ml-100k/results/UserItemExpDBPedia/results")
     dire="./datasets/ml-100k/results/UserItemExpDBPedia/CSV/comparisonAlg/"
     cmd = "Rscript ./scripts/RcomparisonAlgFriedman "+dire+" ././datasets/ml-100k/results/UserItemExpDBPedia/results/ComparisonAlgFriedman"
     subprocess.call(cmd, shell=True)
