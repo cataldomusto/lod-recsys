460e00d93ec67bb405370b4a62be17b1dd774c2e
simoruty <simoruty@gmail.com>
2015-06-30 16:22:03 +0200

    added Random and Popular FS

--- scripts/statProp.py
+++ scripts/statProp.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 import time
 import subprocess
+import sys
+import os
+import shutil
+from time import gmtime, strftime, localtime
+from datetime import datetime
 
 # inserire nel file "all_prop" tutte le proprieta da estrarre
 # output: mapping/*datasetname*/stat indicante la frequenza di ogni proprieta estratta dalle resoource
 
 def extractPropertiesStat(datasets):
-    for dataset in datasets:
-        splitting = range(0, 8)
-        for count in splitting:
-            cmd = "java -cp lodrecsys.jar di.uniba.it.lodrecsys.utils.mapping.MappingStats " + dataset + " " + str(
-                count) + " &"
-            subprocess.call(cmd, shell=True)
-        print time.strftime("%Y-%m-%d %H:%M") + " " + dataset + " properties created."
+	for dataset in datasets:
+		splitting=range(0,8)
+		for count in splitting:
+			cmd = "java -cp lodrecsys.jar di.uniba.it.lodrecsys.utils.mapping.MappingStats " + dataset + " "+str(count)+" &"
+			subprocess.call(cmd, shell=True)
+		print time.strftime("%Y-%m-%d %H:%M") + " "+dataset+" properties created."
